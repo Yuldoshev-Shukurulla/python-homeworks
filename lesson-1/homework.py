5cd04b223bbe0cfec0ec3e186b3bb3e92385b1f1
Shukurulla Yuldashev <yuldashevshukurulla2801@gmail.com>
2026-05-31 16:33:03 +0500

    first lesson

---
+++ lesson-1/homework.py
@@ -0,0 +1,12 @@
+# 1. Why do you think companies analyze large volumes of data?
+# They try to analyze their business and make decicsion that can boost their profit
+# 2. If analyzing and sorting large data manually in Excel is difficult, how do you think Python can help solve this problem?
+# By automating boring tasks
+# 3. Imagine you work at a sales company that receives data about 10,000 customer transactions daily. How would you analyze this data?
+# By using tools like sql, python and power BI I would create a dashboard that refreshses daily and gives live data.
+# 4. In your opinion, what tasks can Python be useful for in BI processes? (e.g., data cleaning, visualization, etc.)
+# I think in data cleaning and in automating this process python is the best.
+# 5. If you wanted to compare a company's profit year by year, how could this be done using Python?
+# calculating both years profit
+# 6. If you don't know Python, what difficulties might you face when working with large datasets?
+#In my own practice I have a problem that I have to repeatedly do some reports every month again and again.
