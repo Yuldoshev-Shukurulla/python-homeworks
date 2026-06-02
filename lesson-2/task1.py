6bb2261ed3415d2d3a3b628119f9f6098c8d795a
Yuldoshev-Shukurulla <yuldashevshukurulla2801@gmail.com>
2026-06-02 20:01:59 +0500

    lesson2

--- lesson-2/task1.py
+++ lesson-2/task1.py
@@ -38,34 +38,34 @@
 
 # ### String Questions:
 
-# 1. Create a program to ask name and year of birth from user and tell them their age.
+# # 1. Create a program to ask name and year of birth from user and tell them their age.
 # name1 = input("Enter your name: ")
 # yearofbirth = float(input("Enter your year of birth: "))
 # print(f"{name1}, you are {2026-yearofbirth} yeart old.")
 
-# 2. Extract car names from this text:
+# # 2. Extract car names from this text:
 # txt = 'LMaasleitbtui'
 # print(txt[::2])
 # print(txt[1::2])
 
-# 3. Write a Python program to:
-#    - Take a string input from the user.
-#    - Print the length of the string.
-#    - Convert the string to uppercase and lowercase.
+# # 3. Write a Python program to:
+# #    - Take a string input from the user.
+# #    - Print the length of the string.
+# #    - Convert the string to uppercase and lowercase.
 # task3 = input("Enter a string: ")
 # print(len(task3))
 # print(task3.upper())
 # print(task3.lower())
 
-# 4. Write a Python program to check if a given string is `palindrome` or not.
+# # 4. Write a Python program to check if a given string is `palindrome` or not.
 # task4 = input("Enter a string: ")
 # cleaned = task4.lower().replace(" ","")
 # if task4 == cleaned[::-1]:
 #     print("String is palindrome.")
 # else: print("String is not palindrome.")
-# > What is a Palindrome String? A string is called a palindrome if the reverse of the string is the same as the original one. Example: “madam”, “racecar”, “12321”.
+# #  > What is a Palindrome String? A string is called a palindrome if the reverse of the string is the same as the original one. Example: “madam”, “racecar”, “12321”.
 
-# 5. Write a program that counts the number of vowels and consonants in a given string.
+# # 5. Write a program that counts the number of vowels and consonants in a given string.
 # task5 = input("Enter a string: ")
 # task5lower = task5.lower()
 # vowels = ("a", "o", "i", "e", "u")
@@ -74,52 +74,64 @@
 #     if x in vowels:
 #         i += 1
 # print(f"In this string there are {len(task5)} letters and {i} of them are vowels and {len(task5)-i} of them are consonants.")
-# 6. Write a Python program to check if one string contains another.
+# # 6. Write a Python program to check if one string contains another.
+# task6 = input("Enter a text: ")
+# task62 = input("Enter a string yo want to check: ")
+# if task62.lower() in task6.lower():
+#     print(f"Success! {task62} found in text.")
+# else: print(f"Sorry, {task62} is not found in text.")
+
+# # 7. Ask the user to input a sentence and a word to replace. Replace that word with another word provided by the user.  
+# # Example:  
+# #    - Input sentence: "I love apples."  
+# #    - Replace: "apples"  
+# #    - With: "oranges"  
+# #    - Output: "I love oranges."
+# task7 = input("Enter a setence: ")
+# task71 = input("Enter a word you want to replace: ")
+# task72 = input("Enter a word you want to add:")
+# task73 = task7.replace(task71, task72)
+# print(task73)
+
+# # 8. Write a program that asks the user for a string and prints the first and last characters of the string. 
+# task8 = input("Enter a string: ")
+# print(task8[0] + " " + task8[-1])
+
+# # 9. Ask the user for a string and print the reversed version of it.
 
 
-# 7. Ask the user to input a sentence and a word to replace. Replace that word with another word provided by the user.  
-# Example:  
-#    - Input sentence: "I love apples."  
-#    - Replace: "apples"  
-#    - With: "oranges"  
-#    - Output: "I love oranges."
+# # 10. Write a program that asks the user for a sentence and prints the number of words in it.  
 
-# 8. Write a program that asks the user for a string and prints the first and last characters of the string.  
-
-# 9. Ask the user for a string and print the reversed version of it.
-
-# 10. Write a program that asks the user for a sentence and prints the number of words in it.  
-
-# 11. Write a program to check if a string contains any digits.  
+# # 11. Write a program to check if a string contains any digits.  
 
 
-# 12. Write a program that takes a list of words and joins them into a single string, separated by a character (e.g., `-` or `,`).  
+# # 12. Write a program that takes a list of words and joins them into a single string, separated by a character (e.g., `-` or `,`).  
 
-# 13. Ask the user for a string and remove all spaces from it.  
+# # 13. Ask the user for a string and remove all spaces from it.  
 
-# 14. Write a program to ask for two strings and check if they are equal or not.  
+# # 14. Write a program to ask for two strings and check if they are equal or not.  
 
-# 15. Ask the user for a sentence and create an acronym from the first letters of each word.  
-#     Example:  
-#     - Input: "World Health Organization"  
-#     - Output: "WHO"  
+# # 15. Ask the user for a sentence and create an acronym from the first letters of each word.  
+#     # Example:  
+#     # - Input: "World Health Organization"  
+#     # - Output: "WHO"  
 
-# 16. Write a program that asks the user for a string and a character, then removes all occurrences of that character from the string.  
+# # 16. Write a program that asks the user for a string and a character, then removes all occurrences of that character from the string.  
 
-# 17. Ask the user for a string and replace all the vowels with a symbol (e.g., `*`).  
+# # 17. Ask the user for a string and replace all the vowels with a symbol (e.g., `*`).  
 
-# 18. Write a program that checks if a string starts with one word and ends with another.  
-#     Example:  
-#     - Input: "Python is fun!"  
-#     - Starts with: "Python"  
-#     - Ends with: "fun!"  
+# # 18. Write a program that checks if a string starts with one word and ends with another.  
+#     # Example:  
+#     # - Input: "Python is fun!"  
+#     # - Starts with: "Python"  
+#     # - Ends with: "fun!"  
 
 
 # ### Boolean Data Type Questions:
-# 1. Write a program that accepts a username and password and checks if both are not empty.
-# 2. Create a program that checks if two numbers are equal and outputs a message if they are.
-# 3. Write a program that checks if a number is positive and even.
-# 4. Write a program that takes three numbers and checks if all of them are different.
-# 5. Create a program that accepts two strings and checks if they have the same length.
-# 6. Create a program that accepts a number and checks if it’s divisible by both 3 and 5.
-# 7. Write a program that checks if the sum of two numbers is greater than 50.8. Create a program that checks if a given number is between 10 and 20 (inclusive)
+# # 1. Write a program that accepts a username and password and checks if both are not empty.
+# # 2. Create a program that checks if two numbers are equal and outputs a message if they are.
+# # 3. Write a program that checks if a number is positive and even.
+# # 4. Write a program that takes three numbers and checks if all of them are different.
+# # 5. Create a program that accepts two strings and checks if they have the same length.
+# # 6. Create a program that accepts a number and checks if it’s divisible by both 3 and 5.
+# # 7. Write a program that checks if the sum of two numbers is greater than 50.8. Create a program that checks if a given number is between 10 and 20 (inclusive)
