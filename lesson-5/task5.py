baec52c65c45ad93c6fc7fd450df9790d7656914
Yuldoshev-Shukurulla <yuldoshevshukurulla2801@gmail.com>
2026-06-08 08:19:47 +0500

    lesson5

--- lesson-5/task5.py
+++ lesson-5/task5.py
@@ -15,7 +15,7 @@
 # Enter a temperature in degrees F: 72
 # 72 degrees F = 22.22 degrees C
 
-# Enter a temperature in degrees C: 37
+# Enter a temperature in degrees C: 37 
 # 37 degrees C = 98.60 degrees F
 
 
