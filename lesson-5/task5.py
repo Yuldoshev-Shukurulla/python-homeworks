24c69a91f716320ced212e37facac54db67c5afc
Yuldoshev-Shukurulla <yuldashevshukurulla2801@gmail.com>
2026-06-09 12:12:45 +0500

    lesson 5

--- lesson-5/task5.py
+++ lesson-5/task5.py
@@ -11,14 +11,25 @@
 
 # Here’s a sample run of the program:
 
-# Enter a temperature in degrees F: 72
+# Enter a temperature in degrees C: 72
 # 72 degrees F = 22.22 degrees C
 
 # Enter a temperature in degrees C: 37 
 # 37 degrees C = 98.60 degrees F
 
 # <hr />
+def convert_cel_to_far(c):
+    Fr = round(c * 9 / 5 +32, 2)
+    print(f'{c} degrees C = {Fr} degrees in F.')
 
+def convert_far_to_cel(f):
+    Cel = round((f - 32) * 5 / 9, 2)
+    print(f'{f} degrees F = {Cel} degrees in C.')
+
+ca = float(input('Enter a temperature in degrees C: '))
+convert_cel_to_far(ca)
+ba = float(input('Enter a temperature in degrees F: '))
+convert_far_to_cel(ba)
 
 
 
@@ -51,6 +62,17 @@
 
 # <hr />
 
+def invest(amount, rate, years):
+    i = 1
+    invest = amount
+    while i <= years:
+        invest = round(invest * (1 + rate), 2)
+        print(f'year {i}: ${invest}.')
+        i += 1
+a = float(input('Enter an initial amount: '))
+b = float(input('Enter an annual percentage rate: '))
+c = float(input('Enter a number of years: '))
+invest(a, b, c)
 # ### Task 3
 
 # A factor of a positive integer n is any positive integer less than or equal to n that divides n with no remainder.
@@ -68,21 +90,29 @@
 # 6 is a factor of 12
 # 12 is a factor of 12
 
+def factors(numa):
+    for i in range(1, numa+1):
+        if numa % i == 0:
+            print(f'{i} is a factor of {numa}')
+numb = int(input('Enter a positive integer: '))
+factors(numb)
+    
+
 
 # ### Task 4
 
 # Write a program that contains the following lists of lists:
 
 # python
-# universities = [
-#     ['California Institute of Technology', 2175, 37704],
-#     ['Harvard', 19627, 39849],
-#     ['Massachusetts Institute of Technology', 10566, 40732],
-#     ['Princeton', 7802, 37000],
-#     ['Rice', 5879, 35551],
-#     ['Stanford', 19535, 40569],
-#     ['Yale', 11701, 40500]
-# ]
+universities = [
+    ['California Institute of Technology', 2175, 37704],
+    ['Harvard', 19627, 39849],
+    ['Massachusetts Institute of Technology', 10566, 40732],
+    ['Princeton', 7802, 37000],
+    ['Rice', 5879, 35551],
+    ['Stanford', 19535, 40569],
+    ['Yale', 11701, 40500]
+]
 
 
 # Define a function, _enrollment_stats()_, that akes, as an input, a list of lists where each individual list contains three elements: (a) the name of a university, (b) the total number of enrolled students, and (c) the annual tuition fees.
@@ -107,8 +137,45 @@
 # Tuition median: $ 39,849
 # ******************************
 
+def enrolment_stats(univ):
+    enrol_val = []
+    tuition = []
+    for i in range(0, len(univ)):
+        enrol_val.append(univ[i][1])
+        tuition.append(univ[i][2])
+    return enrol_val, tuition
+
+def mean(mn):
+    return sum(mn)/len(mn)
+def median(md):
+    md.sort()
+    lmd = len(md)
+    if lmd % 2 != 0:
+        return md[lmd // 2]
+    else:
+        return (md[(lmd // 2) - 1] + md[lmd // 2]) / 2
+nums, fees = enrolment_stats(universities)
+meanen = mean(nums)
+meaden = median(nums)
+meant = mean(fees)
+meadt = median(fees)
+print(f'Total students: {sum(nums):,}')
+print(f'Total tuition: $ {sum(fees):,}')
+print(f'Student mean: {meanen:,.2f}')
+print(f'Student median: {meaden:,}')
+print(f'Tuition mean: $ {meant:,.2f}')
+print(f'Tuition median: $ {meadt:,}')
+
+
 
 # ---
 
 # ### Task 5
 # Define a function `is_prime(n)` which returns `True` if the given $n$ ($n$ > 0) is _prime number_, otherwise returns `False`.
+def is_prime(n):
+    if n < 2:
+        print(False)
+    else:
+        print(not any(n%i == 0 for i in range(2, n)))
+abd = int(input('Enter a number you want to check: '))
+is_prime(abd)
